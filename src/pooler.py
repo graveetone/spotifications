f9358265bfc3b2c99b932560e4feb4543323eed6
graveetone <zastavskijs@gmail.com>
2025-09-24 18:04:26 +0300

    add dummy port binder for render.com deployment

--- src/pooler.py
+++ src/pooler.py
@@ -9,6 +9,7 @@ from models import NotificationKeyboardButton
 from clients.telegram_client import TelegramClient
 from constants import SPOTIFICATIONS_PLAYLIST_LINK, SPOTIFICATIONS_PLAYLIST_ID
 from loguru import logger
+import dummy_port_binder
 
 load_dotenv()
 spotipy_client = SpotipyClient(spotipy_client=get_spotify_proxy())
